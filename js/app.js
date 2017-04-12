ca04113e91d0f1e471091a4c599be1428d65c2eb
harinderkaur <kaurh1013@gmil.com>
2017-04-11 20:44:20 -0400

    switch

---
+++ js/app.js
@@ -0,0 +1,67 @@
+//1
+var Age = prompt("What is Your Age");
+switch (Age) {
+    case '14':
+        console.log("Coupon 1");
+        break;
+    case '21':
+        console.log("Coupon 2");
+        break;
+    case '30':
+        console.log("Coupon 3");
+        break;
+    case '50':
+        console.log("No coupon")
+}
+//2
+var Day = prompt("Enter a Day")
+switch (Day) {
+    case 'Monday':
+        console.log("We serve Pasta")
+        break;
+    case 'Tuesday':
+        console.log("We serve Pizza")
+        break;
+    case 'Friday':
+        console.log("We serve Tacos")
+}
+//3
+var Grade = prompt("Enter your Grades")
+switch (Grade) {
+    case 'A++':
+        console.log("Excellent");
+        break;
+    case 'A+':
+        console.log("Good job");
+        break;
+    case 'B':
+        console.log("Average");
+        break;
+}
+var Year = prompt("Enter the Year")
+switch (Year) {
+    case '2009':
+        console.log("Benjamin");
+        break;
+    case '2010':
+        console.log("Family");
+        break;
+    case '2011':
+        console.log("Lock");
+        break;
+    case '2012':
+        console.log("Animals");
+        break;
+    case '2013':
+        console.log("Real");
+        break;
+    case '2014':
+        console.log("Train");
+        break;
+    case '2015':
+        console.log("Route");
+        break;
+    case '2016':
+        console.log("Slumdog");
+        break;
+}
